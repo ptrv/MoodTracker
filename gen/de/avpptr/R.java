0066bc5fce3cbe32ff4310f98d8ce1f85bc62d3d
lokijuhz <mail@petervasil.net>
2010-12-17 03:10:18 +0100

    Some gui changes.

--- gen/de/avpptr/R.java
+++ gen/de/avpptr/R.java
@@ -20,20 +20,36 @@ public final class R {
         public static final int tab_today=0x7f020006;
     }
     public static final class id {
-        public static final int Button01=0x7f050006;
-        public static final int EditText01=0x7f050005;
-        public static final int SeekBar01=0x7f050002;
-        public static final int SeekBar02=0x7f050003;
-        public static final int SeekBar03=0x7f050004;
-        public static final int TextView01=0x7f050000;
-        public static final int TextView02=0x7f050001;
+        public static final int Button01=0x7f05000a;
+        public static final int EditText01=0x7f050009;
+        public static final int LinearLayout01=0x7f050003;
+        public static final int LinearLayout02=0x7f050002;
+        public static final int ScrollView01=0x7f050001;
+        public static final int SeekBar01=0x7f050006;
+        public static final int TextView11=0x7f050004;
+        public static final int TextView12=0x7f050005;
+        public static final int TextView21=0x7f050007;
+        public static final int TextView22=0x7f050008;
+        public static final int TextViewTitle=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int today_layout=0x7f030001;
+        public static final int today_layou2=0x7f030001;
+        public static final int today_layout=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int labelAngry=0x7f04000b;
+        public static final int labelCalm=0x7f040008;
+        public static final int labelContent=0x7f04000c;
+        public static final int labelEnergetic=0x7f040006;
+        public static final int labelHappy=0x7f040004;
+        public static final int labelRelaxed=0x7f04000a;
+        public static final int labelSad=0x7f040003;
+        public static final int labelStressed=0x7f040009;
+        public static final int labelTired=0x7f040005;
+        public static final int labelTitle=0x7f040002;
+        public static final int labelWorried=0x7f040007;
     }
 }
